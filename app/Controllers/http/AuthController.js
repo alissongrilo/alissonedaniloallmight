759e9c6423bc6d95989a73a7b76ffd6948fcd861
alissongrilo <alissonmiguel197@gmail.com>
2020-12-03 18:04:49 -0300

    apiv2

--- app/Controllers/http/AuthController.js
+++ app/Controllers/http/AuthController.js
@@ -1,17 +1,18 @@
-"use strict"
+"use strict";
 
 const User = use("App/Models/User");
 
 class AuthController {
-    async register({ request }) {
-        const data = request.only(["username", "email", "password"])
-        const user = await User.create(data);
-        return user;
-    }
-
-    async authenticate({ request, auth}){
-        const {email, password} = request.all();
-        const token = await auth.attempt(email, password);
-        return token;
-    }
+  async register({ request }) {
+    const data = request.only(["username", "email", "password"]);
+    const user = await User.create(data);
+    return user;
+  }
+  async authenticate({ request, auth }) {
+    const { email, password } = request.all();
+    const token = await auth.attempt(email, password);
+    return token;
+  }
 }
+
+module.exports = AuthController;
