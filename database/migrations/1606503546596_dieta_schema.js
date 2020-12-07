9785a2a7847ec683d3011978bc6e45f5b7d707a2
alissongrilo <alissonmiguel197@gmail.com>
2020-12-07 12:35:07 -0300

    concertando

--- database/migrations/1606503546596_dieta_schema.js
+++ database/migrations/1606503546596_dieta_schema.js
@@ -6,16 +6,16 @@ class DietaSchema extends Schema {
   up () {
     this.create('Dieta', (table) => {
       table.increments()
-      table.string('Finalidade', 45).notNullable().unique()
-      table.time('Horario', 4).notNullable().unique()
-      table.string('Alimento',45).notNullable().unique()
-      .integer("id_user")
-      .unsigned()
-      .references("id")
-      .inTable("users")
-      .onUpdate("cascade")
-      .onDelete("cascade")
-      .notNullable();
+      table.time('Horario', 4).notNullable()
+      table.string('Alimento',45).notNullable()
+      table
+        .integer("id_user")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onUpdate("cascade")
+        .onDelete("cascade")
+        .notNullable()
       table.timestamps()
     })
   }
--- database/migrations/1606503648690_treino_schema.js
+++ database/migrations/1606503648690_treino_schema.js
@@ -6,16 +6,17 @@ class TreinoSchema extends Schema {
   up () {
     this.create('Treino', (table) => {
       table.increments()
-      table.string('Finalidade', 45).notNullable().unique()
-      table.string('Exercicio', 45).notNullable().unique()
-      table.string('Repeticoes', 2).notNullable().unique()
-      .integer("id_user")
-      .unsigned()
-      .references("id")
-      .inTable("users")
-      .onUpdate("cascade")
-      .onDelete("cascade")
-      .notNullable();
+      table.string('Finalidade', 45).notNullable()
+      table.string('Exercicio', 45).notNullable()
+      table.string('Repeticoes', 2).notNullable()
+      table
+        .integer("id_user")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onUpdate("cascade")
+        .onDelete("cascade")
+        .notNullable()
       table.timestamps()
     })
   }
